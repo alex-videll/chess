443d8d6449a5187afbd7cb2273d806f7dc1b09fb
vgeroim <vgeroim.qa@gmail.com>
2019-11-03 03:40:01 +0000

    create Piece model and pieces table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_03_030429) do
+ActiveRecord::Schema.define(version: 2019_11_03_033502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pieces", force: :cascade do |t|
+    t.string "name"
+    t.string "start_position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/pieces.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :piece do
+    name { "MyString" }
+    start_position { "MyString" }
+  end
+end
