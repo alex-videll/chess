ce3a7c79cfb84a90d0bfab19cbeee08839061fab
vgeroim <vgeroim.qa@gmail.com>
2019-11-03 23:06:41 +0000

    add Result model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_03_224337) do
+ActiveRecord::Schema.define(version: 2019_11_03_230429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2019_11_03_224337) do
   create_table "pieces", force: :cascade do |t|
     t.string "name"
     t.string "starting_position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "results", force: :cascade do |t|
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
