6b186b47b9c7d3ecf2e704934ccc6aa8713d132c
Alexandra Videll <alexandra.videll@gmail.com>
2019-11-09 17:55:40 +0000

    pieces model started

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -1,2 +1,20 @@
 class ChessPiece < ApplicationRecord
+
+    class King < ChessPiece
+    end
+
+    class Queen < ChessPiece
+    end
+
+    class Bishop < ChessPiece
+    end
+
+    class Knight < ChessPiece
+    end
+
+    class Rook < ChessPiece
+    end
+
+    class Pawn < ChessPiece
+    end
 end
