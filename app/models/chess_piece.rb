42e7fee99c8d307bd2336eb6923bc720f5cb49cb
Alexandra Videll <alexandra.videll@gmail.com>
2019-11-10 20:39:51 +0000

    pseudo code for valid_move king

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -1,6 +1,18 @@
 class ChessPiece < ApplicationRecord
 
     class King < ChessPiece
+        def valid_move?(x_position, y_position)
+          if x_position = x_position and y_position = y+1
+            return true
+          elsif x_position = x_position and y_position = y-1
+            return true
+          elsif y_position = y_position and x_position = x+1
+            return true
+          elsif y_position = y_position and x_position = x-1
+            return true
+          else return false
+          end
+        end
     end
 
     class Queen < ChessPiece
