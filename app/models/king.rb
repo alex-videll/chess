8a9533b925257959416c98e5c6d5a107b937ca63
Alexandra Videll <alexandra.videll@gmail.com>
2019-11-15 02:23:10 +0000

    valid move for king

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,6 +1,8 @@
 class King < ChessPiece
-    def valid_move? (x, y)
-        if |x_position - x| == 1 && |y_position - y| == 1
+    def valid_move? (cur, dest)
+        if (|dest.x - cur.x| == 1 || |dest.x - cur.x| == 0)
+             && (|dest.y - cur.y| == 1 || |dest.y - cur.y| == 0) 
+             && dest.x < 7 && dest.y <7 && dest.x >=0 && dest.y >=0 
             return true
         else
             return false
